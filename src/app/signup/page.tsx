77667a3bbccb6fc0ccf30ff8648e979f26b009b2
Cyros-Sachin <sachinkc4456@gmail.com>
2026-04-24 23:45:45 +0530

    check

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -3,6 +3,7 @@ import Footer from "@/components/Footer";
 import Navbar from "@/components/Navbar";
 
 export default function SignupPage() {
+  console.log(process.env.NEXT_PUBLIC_API_URL)
   return (
     <div className="min-h-screen flex flex-col">
       <Navbar />
